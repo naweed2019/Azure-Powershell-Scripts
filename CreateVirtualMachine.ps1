6b245c5ac1716cefe6bb9659de7e94a0d12e3073
naweed2019 <naweed99@hotmail.com>
2021-05-06 11:00:08 +0100

    commiting changes

---
+++ CreateVirtualMachine.ps1
@@ -0,0 +1,3 @@
+New—AzVM -ResouceGroupName "myResourceGroup" -Name "myVM —Virtua1NetworkName "myVnet" —SubnetName "mySubnet" —SecuritygroupName "mysecuritycroup" —PubliclpAddressName "myPub1icIpAddress" -openports 80,3389 
+
+
